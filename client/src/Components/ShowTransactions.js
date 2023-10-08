9f3dd54c397a0c0165e577efc258e7820176e4ce
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-10-08 16:44:13 -0400

    feat: display transactions and organize by dates

--- client/src/Components/ShowTransactions.js
+++ client/src/Components/ShowTransactions.js
@@ -12,16 +12,47 @@ function ShowTransactions() {
     const response = await fetch(data);
 
     var responseText = await response.text();
-    updateTransactions(responseText.split("\n"));
+    var responseArray = responseText.split("\n");
+
+    var transactionsOrganized = [];
+    var transactionsInOneDay = [];
+
+    //organize by date
+    for (var i = 0; i < responseArray.length; i++) {
+      if (i > 0) {
+        if (
+          i % 2 === 0 &&
+          responseArray[i].split(" ")[0] !== responseArray[i - 2].split(" ")[0]
+        ) {
+          transactionsOrganized.push(transactionsInOneDay);
+          transactionsInOneDay = [];
+          transactionsInOneDay.push(responseArray[i]);
+        } else {
+          transactionsInOneDay.push(responseArray[i]);
+        }
+      } else {
+        transactionsInOneDay.push(responseArray[i]);
+      }
+    }
+
+    updateTransactions(transactionsOrganized);
   }
 
+  //look into keys
   return (
     <section id="transactions">
       <h1>ShowTransaction</h1>
 
-      {transactions.map((item) => (
-        <h4>{item}</h4>
-      ))}
+      {transactions.map((items) => {
+        return (
+          <div>
+            {items.map((subItems) => {
+              return <h4>{subItems}</h4>;
+            })}
+            <h4>-----</h4>
+          </div>
+        );
+      })}
     </section>
   );
 }
