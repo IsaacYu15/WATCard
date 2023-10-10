c1055c4273371014349530dd327d55458cb12658
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-10-09 21:29:41 -0400

    feat: add transactions into pern stack

--- client/src/Components/ShowTransactions.js
+++ client/src/Components/ShowTransactions.js
@@ -2,17 +2,40 @@ import React, { useEffect, useState } from "react";
 import data from "../data.txt";
 
 function ShowTransactions() {
+  const [rawData, updateRawData] = useState([]);
   const [transactions, updateTransactions] = useState([]);
 
   useEffect(() => {
     fileToArr();
   }, []);
 
+  //"/r bug occuring"
+  const submitTransactions = async (e) => {
+    e.preventDefault(); //stop refresh
+
+    try {
+      for (var i = 0; i < 4; i += 2) {
+        const response = await fetch("http://localhost:5000/transactions", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            date: rawData[i],
+            amount: rawData[i + 1],
+          }),
+        });
+        console.log(response);
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   async function fileToArr() {
     const response = await fetch(data);
 
     var responseText = await response.text();
     var responseArray = responseText.split("\n");
+    updateRawData(responseArray);
 
     var transactionsOrganized = [];
     var transactionsInOneDay = [];
@@ -41,6 +64,7 @@ function ShowTransactions() {
   //look into keys
   return (
     <section id="transactions">
+      <button onClick={submitTransactions}>SUBMIT TRANSACTIONS</button>
       <h1>ShowTransaction</h1>
 
       {transactions.map((items) => {
--- server/index.js
+++ server/index.js
@@ -11,62 +11,73 @@ app.use(express.json());
 
 //create a transaction
 app.post("/transactions", async (req, res) => {
-    try {
-        const { date, amount } = req.body;
-        const newTransaction = await pool.query("INSERT INTO transactions (date, amount) VALUES ($1, $2) RETURNING *", [date, amount]);
+  try {
+    const { date, amount } = req.body;
+    const newTransaction = await pool.query(
+      "INSERT INTO transactions (date, amount) VALUES ($1, $2) RETURNING *",
+      [date, amount]
+    );
 
-        res.json(newTransaction.rows[0]);
-    } catch (err) {
-        console.error(err.message);
-    }
+    res.json(newTransaction.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //get all transaction
 app.get("/transactions", async (req, res) => {
-    try {
-        const allTransactions = await pool.query ("SELECT * FROM transactions");
-        res.json(allTransactions.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
+  try {
+    const allTransactions = await pool.query("SELECT * FROM transactions");
+    res.json(allTransactions.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
 });
 
 //get a specific transaction
-app.get("/transactions/:id", async(req, res) => {
-    try {
-        const {id}= req.params;
-        const transactions = await pool.query("SELECT * FROM transactions WHERE transaction_id = $1", [id]);
+app.get("/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const transactions = await pool.query(
+      "SELECT * FROM transactions WHERE transaction_id = $1",
+      [id]
+    );
 
-        res.json(transactions.rows[0]);
-
-    } catch (err) {
-        console.log(err.message);
-    }
+    res.json(transactions.rows[0]);
+  } catch (err) {
+    console.log(err.message);
+  }
 });
 
 //update a transaction
-app.put("/transactions/:id", async(req, res) => {
-    try {
-        const {id} = req.params;
-        const { date, amount } = req.body;
-        const updateTransaction = await pool.query("UPDATE transactions SET amount = $1 WHERE transaction_id = $2", [amount, id]);
-        res.json ("updated transaction");
-    } catch (err) {
-        console.log(err.message);
-    }
-})
+app.put("/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { date, amount } = req.body;
+    const updateTransaction = await pool.query(
+      "UPDATE transactions SET amount = $1 WHERE transaction_id = $2",
+      [amount, id]
+    );
+    res.json("updated transaction");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 //delete a transaction
-app.delete("/transactions/:id", async(req,res) => {
-    try {
-        const {id} = req.params;
-        const delteTransaction = await pool.query("DELETE FROM transactions WHERE transaction_id = $1", [id]);
-        res.json("deleted transaction");
-    } catch (err) {
-        console.log(err.message);
-    }
-})
+app.delete("/transactions/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTransaction = await pool.query(
+      "DELETE FROM transactions WHERE transaction_id = $1",
+      [id]
+    );
+    res.json("deleted transaction");
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 app.listen(5000, () => {
-    console.log("server started on port 5000");
-})
+  console.log("server started on port 5000");
+});
